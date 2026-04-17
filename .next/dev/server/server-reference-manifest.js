24262726714c3e729df0ae4b2e8b02fa68f2e70d
JaoFraccaroli <joaofraccarolifilho@gmail.com>
2026-04-17 20:58:44 -0300

    Quarto

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"64nHwInzTkSRPALPMRqRihbXy5RwgHUJ43K+S1XSxj0=\"\n}"
--- .next/static/chunks/0uijl0248b86x.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/", [
-  "static/chunks/0kl4ko6kck6~c.js",
-  "static/chunks/0hxzmoo_nlyk..js",
-  "static/chunks/turbopack-0wqs7od2m3rl3.js"
-])
